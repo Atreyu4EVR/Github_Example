8d41a5a8ab7ad9064f9dff2c11ad26e51cd9fa59
Ron Vallejo <atreyu4evr@pm.me>
2024-11-26 13:30:23 -0700

    first commit

---
+++ file_example.py
@@ -0,0 +1,12 @@
+# Simple loop through a range of numbers
+for i in range(5):
+    print(i)  # This will print numbers 0 through 4
+
+# Loop through a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# Loop with range starting at 1 and ending at 5
+for num in range(1, 6):
+    print(f"Number: {num}")
